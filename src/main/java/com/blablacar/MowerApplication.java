520eecd8edf16f88c7c9d274c7de54538abaa8e6
mehdi-aouadi <mehdi.aouadi@gmail.com>
2019-10-12 10:32:47 +0200

    [SETUP] Init project

---
+++ src/main/java/com/blablacar/MowerApplication.java
@@ -0,0 +1,13 @@
+package com.blablacar;
+
+/**
+ * Hello world!
+ *
+ */
+public class MowerApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
